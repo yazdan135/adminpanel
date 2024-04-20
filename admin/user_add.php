0186fc3366f912f74c0285138e91057a863eace9
M.Rizwan <rizwan02091986@gmail.com>
2024-04-20 20:35:43 +0500

    20-04-2024

--- admin/user_add.php
+++ admin/user_add.php
@@ -22,54 +22,19 @@ include("connection.php");
 
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title">Horizontal Form</h4>
+                <h4 class="card-title">Role Form</h4>
             </div>
             <div class="card-body">
                 <div class="basic-form">
                     <form>
 
                         <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label>First Name</label>
-                                <input type="text" class="form-control" placeholder="1234 Main St">
+                            <div class="form-group col-md-12">
+                                <label>Role Name</label>
+                                <input type="text" name="role" class="form-control" placeholder="Enter your Role Name">
                             </div>
-                            <div class="form-group col-md-6">
-                                <label>Email</label>
-                                <input type="email" class="form-control" placeholder="Email">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>Password</label>
-                                <input type="password" class="form-control" placeholder="Password">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>City</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-4">
-                                <label>State</label>
-                                <select id="inputState" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>Option 1</option>
-                                    <option>Option 2</option>
-                                    <option>Option 3</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-2">
-                                <label>Zip</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox">
-                                <label class="form-check-label">
-                                    Check me out
-                                </label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Sign in</button>
+                            
+                        <button type="submit" name="submit" class="btn btn-primary">Add Role</button>
                     </form>
                 </div>
             </div>
