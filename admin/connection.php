3076438edb7e7b7be143fe3b8777c51520e602f0
M.Rizwan <rizwan02091986@gmail.com>
2024-04-20 20:31:04 +0500

    20-04-2024

---
+++ admin/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$conn = new mysqli("localhost", "root" , "" , "bookstore2307g1");
+
+if(!$conn){
+    die("Connection Failed");
+}
+
+?>
--- admin/user_add.php
+++ admin/user_add.php
@@ -1,5 +1,6 @@
 <?php
 include("header.php");
+include("connection.php");
 ?>
 
 <div class="content-body">
